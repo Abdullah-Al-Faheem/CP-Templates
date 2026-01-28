87d4be4cfa39dd001e00886166cbc345df1ee976
Abdullah Al Fahim <138691546+Abdullah-Al-Faheem@users.noreply.github.com>
2026-01-28 12:56:22 +0600

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,19 @@
+auto ok = [&](int mid) 
+{
+    // return true/false;
+};
+
+int lft = 0, rgt = n, mid, ans = 0;
+while(lft<=rgt)
+{
+    mid = lft + (rgt-lft)/2;
+    // if(ok(mid))
+    // {
+    //     ans = mid;
+    //     lft = mid+1;
+    // }
+    // else
+    // {
+    //     rgt = mid-1;
+    // }
+}
