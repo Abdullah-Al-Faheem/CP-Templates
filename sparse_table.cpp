52d1506cb2b9fd97bcc691511bfb1d17e9560a2b
Abdullah Al Fahim <138691546+Abdullah-Al-Faheem@users.noreply.github.com>
2026-02-24 22:16:30 +0600

    Create sparse_table.cpp

---
+++ sparse_table.cpp
@@ -0,0 +1,24 @@
+const int M = 1e5+5; // change here
+int num[M];
+int sparse[M][24]; // change here
+
+//call with build(n)
+void build(int n)
+{
+    int m = log2l(n); // may give precision error
+
+    for (int i = 1; i <= n; i++) sparse[i][0]=num[i];
+
+    for (int j = 1; j <= m; j++)
+        for (int i = 1; i +(1<<j) -1 <= n; i++)
+            sparse[i][j]= min (sparse[i][j-1], sparse[i+(1<<(j-1))][j-1]); // change here
+}
+
+// call with query(L,R)
+int query(const int &L, const int &R)
+{
+    int len = R-L+1;
+    int j = log2l(len); // may give precision error
+
+    return min(sparse[L][j], sparse[R -(1<<j) +1][j]) ; // change here
+}
