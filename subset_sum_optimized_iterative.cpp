2fe183bf8d22974e53e2841526f8a02f8c366550
Abdullah Al Fahim <138691546+Abdullah-Al-Faheem@users.noreply.github.com>
2026-02-25 02:59:19 +0600

    Update comments to clarify subset growth concerns
    Added a note about potential subset growth in comments.

--- subset_sum_optimized_iterative.cpp
+++ subset_sum_optimized_iterative.cpp
@@ -1,6 +1,7 @@
 // time complexity -> O(n* target_sum)
 // space complexity -> O(target_sum)
 // works for positive integer array only
+// number of subsets can grow very fast, may need to use boolean dp or mod
 int subsetSum(vector<int>& arr, int target) 
 {
     vector<int> dp(target + 1, 0);
